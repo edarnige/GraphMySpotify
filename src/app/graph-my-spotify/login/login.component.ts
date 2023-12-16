7e243c232d003fac3b68c85dd0dbfe39d9a92427
edarnige <edendarnige@gmail.com>
2023-12-16 22:56:31 +0100

    Create login popup and form

--- src/app/graph-my-spotify/login/login.component.ts
+++ src/app/graph-my-spotify/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Make sure to import FormsModule
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  openPopup = false;
+  email = '';
+  password = '';
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+
+  openForm() {
+    this.openPopup = true;
+    this.cdr.detectChanges(); // Manually trigger change detection
+  }
+  
+  closeForm() {
+    this.openPopup = false;
+  }
+
+  loginFormSubmit() {
+    // Handle form submission logic here
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Add your authentication logic or API calls here
+  }
 
 }
